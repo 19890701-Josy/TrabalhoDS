964e773b3b961951f2938c8f5932d908132b8d8f
Joselia Dias <joseliadiassales2024@outlook.com>
2025-06-18 19:30:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    const mensagem = "Olá! Que O Senhor Jesus seja sempre a nossa melhor companhia!";
+    const div = document.getElementById("mensagem");
+    div.textContent = mensagem;
+}
